3999d20b73a32779720795e3185b537c7b309869
your-shikshak <admin@yourshikshak.in>
2026-04-20 00:40:48 +0530

    Update Arduino code with dual servo gates and buzzer functionality

--- SmartParkingArduino/SmartParkingArduino.ino
+++ SmartParkingArduino/SmartParkingArduino.ino
@@ -2,32 +2,42 @@
 
 const byte ENTRY_SENSOR_PIN = 2;
 const byte EXIT_SENSOR_PIN = 3;
-const byte SERVO_PIN = 4;
+const byte ENTRY_SERVO_PIN = 4;
+const byte EXIT_SERVO_PIN = 5;
+const byte BUZZER_PIN = 6;
 
 int totalSlots = 4;
 int availableSlots = 4;
 
 const int IR_ACTIVE_STATE = LOW;
 
-const int GATE_OPEN_ANGLE = 0;
-const int GATE_CLOSE_ANGLE = 100;
+const int ENTRY_GATE_OPEN_ANGLE = 0;
+const int ENTRY_GATE_CLOSE_ANGLE = 100;
 
-const unsigned long GATE_OPEN_TIME = 1000;
+// Exit servo direction is reversed compared with entry servo.
+const int EXIT_GATE_OPEN_ANGLE = 100;
+const int EXIT_GATE_CLOSE_ANGLE = 0;
+
 const unsigned long DEBOUNCE_TIME = 60;
 const unsigned long SENSOR_COOLDOWN = 1500;
-const unsigned long SLOT_SEND_INTERVAL = 500;
+const unsigned long GATE_OPEN_TIME = 2000;
+const unsigned long BEEP_TIME = 200;
+const unsigned long BEEP_GAP = 120;
 
-Servo gateServo;
+Servo entryServo;
+Servo exitServo;
 
 bool lastEntryState = false;
 bool lastExitState = false;
 
 unsigned long lastEntryTriggerTime = 0;
 unsigned long lastExitTriggerTime = 0;
-unsigned long lastSlotSendTime = 0;
 
-bool gateIsOpen = false;
-unsigned long gateOpenedTime = 0;
+bool entryGateOpen = false;
+bool exitGateOpen = false;
+
+unsigned long entryGateOpenedTime = 0;
+unsigned long exitGateOpenedTime = 0;
 
 bool isSensorTriggered(byte pin) {
   if (digitalRead(pin) != IR_ACTIVE_STATE) {
@@ -50,25 +60,53 @@ void sendSlots() {
   Serial.println(availableSlots);
 }
 
-void openGate() {
-  gateServo.write(GATE_OPEN_ANGLE);
-  gateIsOpen = true;
-  gateOpenedTime = millis();
+void openEntryGate() {
+  entryServo.write(ENTRY_GATE_OPEN_ANGLE);
+  entryGateOpen = true;
+  entryGateOpenedTime = millis();
 }
 
-void closeGateIfNeeded() {
-  if (gateIsOpen && millis() - gateOpenedTime >= GATE_OPEN_TIME) {
-    gateServo.write(GATE_CLOSE_ANGLE);
-    gateIsOpen = false;
+void openExitGate() {
+  exitServo.write(EXIT_GATE_OPEN_ANGLE);
+  exitGateOpen = true;
+  exitGateOpenedTime = millis();
+}
+
+void closeGatesIfNeeded() {
+  unsigned long currentTime = millis();
+
+  if (entryGateOpen && currentTime - entryGateOpenedTime >= GATE_OPEN_TIME) {
+    entryServo.write(ENTRY_GATE_CLOSE_ANGLE);
+    entryGateOpen = false;
   }
+
+  if (exitGateOpen && currentTime - exitGateOpenedTime >= GATE_OPEN_TIME) {
+    exitServo.write(EXIT_GATE_CLOSE_ANGLE);
+    exitGateOpen = false;
+  }
+}
+
+void beepOnce() {
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(BEEP_TIME);
+  digitalWrite(BUZZER_PIN, LOW);
+}
+
+void beepTwice() {
+  beepOnce();
+  delay(BEEP_GAP);
+  beepOnce();
 }
 
 void handleEntry() {
   if (availableSlots > 0) {
-    openGate();
+    openEntryGate();
     availableSlots--;
+
     Serial.println("ENTRY");
     sendSlots();
+
+    beepOnce();
   } else {
     Serial.println("FULL");
     sendSlots();
@@ -78,19 +116,31 @@ void handleEntry() {
 void handleExit() {
   if (availableSlots < totalSlots) {
     availableSlots++;
-  }
 
-  openGate();
-  Serial.println("EXIT");
-  sendSlots();
+    openExitGate();
+
+    Serial.println("EXIT");
+    sendSlots();
+
+    beepTwice();
+  } else {
+    // Parking is already empty, so ignore false exit triggers.
+    sendSlots();
+  }
 }
 
 void setup() {
   pinMode(ENTRY_SENSOR_PIN, INPUT_PULLUP);
   pinMode(EXIT_SENSOR_PIN, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
 
-  gateServo.attach(SERVO_PIN);
-  gateServo.write(GATE_CLOSE_ANGLE);
+  digitalWrite(BUZZER_PIN, LOW);
+
+  entryServo.attach(ENTRY_SERVO_PIN);
+  exitServo.attach(EXIT_SERVO_PIN);
+
+  entryServo.write(ENTRY_GATE_CLOSE_ANGLE);
+  exitServo.write(EXIT_GATE_CLOSE_ANGLE);
 
   Serial.begin(9600);
   sendSlots();
@@ -115,10 +165,5 @@ void loop() {
   lastEntryState = entryState;
   lastExitState = exitState;
 
-  closeGateIfNeeded();
-
-  if (currentTime - lastSlotSendTime >= SLOT_SEND_INTERVAL) {
-    lastSlotSendTime = currentTime;
-    sendSlots();
-  }
+  closeGatesIfNeeded();
 }
